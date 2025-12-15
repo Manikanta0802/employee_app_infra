0574bc28053ddb5f3acdac1dd899cc787e704efc
Manikanta0802 <n151310@rguktn.ac.in>
2025-12-15 10:37:44 +0000

    testing infra

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,9 +4,10 @@ resource "aws_instance" "app_server" {
   key_name                    = var.key_pair_name
   subnet_id                   = var.public_subnet_id
   vpc_security_group_ids      = [var.ec2_sg_id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   associate_public_ip_address = true
   lifecycle {
-    recreate_before_destroy = true
+    create_before_destroy = true
   }
 
   user_data = templatefile("${path.module}/userdata.sh", {
@@ -25,3 +26,28 @@ resource "aws_instance" "app_server" {
     Project = "MiniProject"
   }
 }
+
+
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "app-server-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "app-server-instance-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
