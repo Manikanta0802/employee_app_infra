12d69df06bd37bef2237dddb86668bfff2b2f768
Manikanta0802 <n151310@rguktn.ac.in>
2025-12-15 12:42:57 +0000

    testing infra

--- modules/ec2/userdata.sh
+++ modules/ec2/userdata.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -eux
+
+echo "user_data" > /var/lib/cloud/instance/sem/config_scripts_user
+
 exec > >(tee /var/log/userdata.log|logger -t userdata) 2>&1
 
 echo "Starting user data script..."
 
-# Update and install Docker on AL2023
+
 sudo dnf update -y
 sudo dnf install docker -y
 
@@ -12,53 +14,31 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 
-# Ensure SSM Agent is installed (Amazon Linux 2023 already has it)
-sudo systemctl enable amazon-ssm-agent
-sudo systemctl start amazon-ssm-agent
-
-# Create App Directory
-mkdir -p /opt/app
-chmod 755 /opt/app
-
-# Creating the deploy.sh script
-cat << 'EOF' > /opt/app/deploy.sh
-#!/bin/bash
-set -eux
-
-echo "Starting Deployment at $(date)"
-
-echo "Logging into GHCR..."
-echo "$GHCR_TOKEN" | docker login ghcr.io -u "$GHCR_USER" --password-stdin
-
-echo "Pulling backend image..."
-docker pull "$BACKEND_IMAGE"
-docker stop employee-backend || true
-docker rm employee-backend || true
-
-docker run -d \
+# Pull Backend Image
+sudo docker pull ${backend_image}
+sudo docker stop employee-backend || true
+sudo docker rm employee-backend || true
+sudo docker run -d \
   --name employee-backend \
   -p 8080:8080 \
-  -e SPRING_DATASOURCE_URL="jdbc:mysql://${DB_HOST}/employee_availability" \
-  -e SPRING_DATASOURCE_USERNAME="${DB_USER}" \
-  -e SPRING_DATASOURCE_PASSWORD="${DB_PASS}" \
+  -e SPRING_DATASOURCE_URL="jdbc:mysql://${db_host}:${db_port}/${db_name}" \
+  -e SPRING_DATASOURCE_USERNAME="${db_user}" \
+  -e SPRING_DATASOURCE_PASSWORD="${db_password}" \
   --restart=always \
-  "$BACKEND_IMAGE"
+  ${backend_image}
 
-echo "Pulling frontend image..."
-docker pull "$FRONTEND_IMAGE"
-docker stop employee-frontend || true
-docker rm employee-frontend || true
+sudo docker pull ghcr.io/manikanta0802/employee-frontend:5
+sudo docker stop employee-frontend || true
+sudo docker rm employee-frontend || true
 
-docker run -d \
+# Pull Frontend Image
+sudo docker pull ${frontend_image}
+sudo docker stop employee-frontend || true
+sudo docker rm employee-frontend || true
+sudo docker run -d \
   --name employee-frontend \
   -p 80:80 \
   --restart=always \
-  "$FRONTEND_IMAGE"
+  ${frontend_image}
 
-echo "Deployment completed at $(date)"
-EOF
-
-# Make deploy script executable
-chmod +x /opt/app/deploy.sh
-
-echo "User-data completed successfully"
+echo "Finished user data script!"
