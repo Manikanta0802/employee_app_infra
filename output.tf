f3c0706b18af8cdc3af211ac1de3e77cbac7de8e
Manikanta0802 <n151310@rguktn.ac.in>
2025-12-18 08:36:53 +0000

    added n8n output

--- output.tf
+++ output.tf
@@ -19,6 +19,9 @@ output "prometheus_url" {
 output "grafana_url" {
   value = "http://${module.monitor_ec2.monitor_public_ip}:3000"
 }
+output "n8n_url" {
+  value = "http://${module.n8n_ec2.n8n_public_ip}:5678"
+}
 
 output "alb_dns_name" {
   value = "http://${module.alb.alb_dns_name}"
