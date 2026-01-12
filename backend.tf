7258779d94ac0f2585520e42fb76d83865e1d527
Manikanta0802 <n151310@rguktn.ac.in>
2026-01-12 10:37:32 +0000

    updated backend setup

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "employees-app-terraform-state"
+    bucket         = "employees-app-terraform-state-1"
     key            = "employees-app/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraform-state-lock-table"
+    dynamodb_table = "terraform-state-lock-table-1"
   }
 }
